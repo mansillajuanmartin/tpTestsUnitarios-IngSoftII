e2ac772728079d184d9f37004cfb4a29e7de9b2d
JuanMartinMansilla <mansillajuanmartin18@gmail.com>
2021-10-20 23:32:28 -0300

    Septimo commit: Se realizaron los 7/7 test del punto 4 del tp.

---
+++ tests/EmpleadoTest.php
@@ -0,0 +1,65 @@
+<?php
+
+	abstract class EmpleadoTest extends \PHPUnit\Framework\TestCase{
+		//Tests 1/7: Nombre Y Apellido
+		public function testSePuedeCrearYObtenerNombreYApellido(){
+			$e = $this->crear();
+			$this->assertEquals("Emanuel Montoto", $e->getNombreApellido());
+		}
+		//Tests 1/7: DNI
+		public function testSePuedeCrearYObtenerDNI(){
+			$e = $this->crear();
+			$this->assertEquals(30, $e->getDNI());
+		}
+		//Tests 1/7: Salario
+		public function testSePuedeCrearyObtenerElSalario(){
+			$e = $this->crear();
+			$this->assertEquals(5000,$e->getSalario());
+		}
+		// Tests 1/7: getSector Y setSector
+		public function testSePuedeModificarElSectorDelEmpleado(){
+			$e=$this->crear();
+			$lugar = "cualquiera";
+			$this->assertEquals("No especificado",$e->getSector());
+			//seteo el sector que le asigno
+			$e->setSector($lugar);
+			//pruebo si se asigno correctamente
+			$this->assertEquals("cualquiera",$e->getSector());
+		}
+		// Test 1/7: __toString
+		public function testSePuedeConvertirElObjetoEnUnaCadena(){
+			$e=$this->crear();
+			$this->assertEquals("Emanuel Montoto 30 5000",$e);
+		}
+        // Test 2/7: Nombre vacio
+		public function testExcepcionSiSeCreaEmpleadoConNombreVacio(){
+			$this->expectException(\Exception::class);
+			$e= $this-> crear($nombre="");
+		}
+		//Test 3/7: Apellido vacio
+		public function testExcepcionSiSeCreaEmpleadoConApellidoVacio(){
+			$this->expectException(\Exception::class);
+			$e= $this-> crear($nombre="Franco", $Apellido="");
+		}
+		//Test 4/7: DNI Vacio
+		public function testExcepcionSiSeCreaEmpleadoConDniVacio(){
+			$this->expectException(\Exception::class);
+			$e= $this-> crear(null, null,$dni="");
+		}
+        //Test 5/7: Salario Vacio
+		public function testExcepcionSiSeCreaEmpleadoConSalarioVacio(){
+			$this->expectException(\Exception::class);
+			$e= $this-> crear(null, null, null,$Salario="");
+		}
+		//Test 6/7: DNI Con letras o caracteres no numericos
+		public function testExcepcionSiSeCreaEmpleadoConDniQueContengaLetras(){
+			$this->expectException(\Exception::class);
+			$e= $this-> crear(null, null, "4kh69");
+		}
+		//Test 7/7: No se especifica el sector
+		public function testSectorNoEspecificadoSiSeDanInstanciaSinEspecificar(){
+			$e= $this-> crear("Franco", "Mitre", 3, 6000);
+			$this->assertEquals("No especificado", $e->getSector());
+		}
+	}
+?>
